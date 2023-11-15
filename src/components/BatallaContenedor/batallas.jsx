1f7c3254b882f69eca13efea8f1a9701ea9d643f
Gianluca <gianluca@tsf.com>
2023-11-15 09:46:30 -0300

    agrega spinner

--- src/components/BatallaContenedor/batallas.jsx
+++ src/components/BatallaContenedor/batallas.jsx
@@ -1,21 +1,29 @@
 import Container from 'react-bootstrap/Container';
 import "./batallas.css"
 import Batalla from './Batalla/batalla';
+import { useState, useEffect } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 
 function BatallasContenedor(){
-    const items = [
-        {titulo: 'batalla 1', descripcion: 'descripcion batalla 1', img: 'src/assets/img/imagen1.jpg'}, 
-        {titulo: 'batalla 2', descripcion: 'descripcion batalla 2', img: 'src/assets/img/imagen2.jpg'},
-        {titulo: 'batalla 3', descripcion: 'descripcion batalla 3', img: 'src/assets/img/img3.png'},
-        {titulo: 'batalla 4', descripcion: 'descripcion batalla 4', img: 'src/assets/img/img4.png'},
-        {titulo: 'batalla 1', descripcion: 'descripcion batalla 1', img: 'src/assets/img/imagen1.jpg'}, 
-        {titulo: 'batalla 3', descripcion: 'descripcion batalla 3', img: 'src/assets/img/img3.png'},
-        {titulo: 'batalla 2', descripcion: 'descripcion batalla 2', img: 'src/assets/img/imagen2.jpg'}
-    ];
+    const [items, setItems] = useState(null); 
+
+    useEffect(() => {
+        setTimeout(() => {
+            setItems([
+                {titulo: 'batalla 1', descripcion: 'descripcion batalla 1', img: 'src/assets/img/imagen1.jpg'}, 
+                {titulo: 'batalla 2', descripcion: 'descripcion batalla 2', img: 'src/assets/img/imagen2.jpg'},
+                {titulo: 'batalla 3', descripcion: 'descripcion batalla 3', img: 'src/assets/img/img3.png'},
+                {titulo: 'batalla 4', descripcion: 'descripcion batalla 4', img: 'src/assets/img/img4.png'},
+                {titulo: 'batalla 1', descripcion: 'descripcion batalla 1', img: 'src/assets/img/imagen1.jpg'}, 
+                {titulo: 'batalla 3', descripcion: 'descripcion batalla 3', img: 'src/assets/img/img3.png'},
+                {titulo: 'batalla 2', descripcion: 'descripcion batalla 2', img: 'src/assets/img/imagen2.jpg'}
+            ]);
+        }, 2500);
+    }, []);
 
     return(
         <Container fluid id='batallaContainer'>
-            <Batalla batallas={items}/>
+            {items ? <Batalla batallas={items}/> : <Spinner animation="grow" variant="warning"/>}
         </Container>
     );
 }
